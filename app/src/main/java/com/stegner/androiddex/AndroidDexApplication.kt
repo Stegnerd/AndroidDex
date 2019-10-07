a5cf0c656c9656a2ccaf96c50eb244728095b288
dstegner <dmstegner@gmail.com>
2019-10-07 16:38:14 -0400

    Initial commit

---
+++ app/src/main/java/com/stegner/androiddex/AndroidDexApplication.kt
@@ -0,0 +1,4 @@
+package com.stegner.androiddex
+
+class AndroidDexApplication {
+}
---
+++ app/src/main/java/com/stegner/androiddex/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.stegner.androiddex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
